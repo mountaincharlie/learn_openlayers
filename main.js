7a935ed49a07e39363f1dfdefe89609b5aebe1b7
Charlie Harland <ciharland@hotmail.co.uk>
2023-01-12 15:39:09 +0000

    style: add comments for understanding features of OpenLayers

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
+import { Map, View } from 'ol';  // importing Map and View classes
+import TileLayer from 'ol/layer/Tile';  // importing basic type of layer
 import OSM from 'ol/source/OSM';
+import Stamen from 'ol/source/Stamen';
+import { fromLonLat } from 'ol/proj';
 
+// creates a new instance of Map
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
-      source: new OSM()
+      // source: new OSM()
+      source: new Stamen({layer: 'watercolor'})  // water colour effect
     })
-  ],
+  ],  // creates new TileLayer
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    // center: fromLonLat([-0.5595, 51.236]),  // [-1.162556, 51.360851] for Hercules House, [-0.5595, 51.236] for roughly my house
+    center: [0,0],
+    zoom: 3
   })
 });

f40d11f74809cf9c88fdbe1df010997365bdf258
Charlie Harland <ciharland@hotmail.co.uk>
2023-01-13 10:34:35 +0000

    refactor/fix: move layers and views into their own vars and include imports to fix layers not showing

--- main.js
+++ main.js
@@ -1,52 +1,92 @@
 import './style.css';
 import { Map, View } from 'ol';  // importing Map and View classes
 import TileLayer from 'ol/layer/Tile';  // importing basic type of layer
+import VectorLayer from 'ol/layer/Vector';
+import VectorImageLayer from 'ol/layer/VectorImage';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
 import OSM from 'ol/source/OSM';  // basic ol map style
 import Stamen from 'ol/source/Stamen';  // for type of TileLayer
 import { fromLonLat } from 'ol/proj';  // for coordinates as lonlat
 import { Geometry } from 'ol/geom';
+import * as olProj from 'ol/proj';
+import Icon from 'ol/style/Icon';
+import {Fill, Stroke, Style} from 'ol/style';
+import CircleStyle from 'ol/style/Circle.js';
 
+
+// my View
+var myView = new View({
+  center: fromLonLat([-1.162556, 51.360851]),  // [-1.162556, 51.360851] for Hercules House, [-0.5595, 51.236] for roughly my house
+  // center: [0,0],
+  zoom: 16
+});
+
+// my layers
+var myBaseLayer = new TileLayer({
+  source: new OSM()
+  // source: new Stamen({layer: 'watercolor'})  // water colour effect
+});
+var myLabelsLayer = new TileLayer({
+  source: new Stamen({layer: 'terrain-labels'})  // adds terrian labels
+});
+
+// all the layers
+var myLayers = [myBaseLayer, myLabelsLayer];
 
 // creates a new instance of Map
 const map = new Map({
   target: 'map',  // must match the id given in the html
-  layers: [
-    new TileLayer({
-      source: new OSM()
-      // source: new Stamen({layer: 'watercolor'})  // water colour effect
-    }),
-    // new TileLayer({
-    //   source: new Stamen({layer: 'terrain-labels'})  // adds terrian labels
-    // }),
-  ],  // creates new TileLayer
-  view: new View({
-    center: fromLonLat([-1.162556, 51.360851]),  // [-1.162556, 51.360851] for Hercules House, [-0.5595, 51.236] for roughly my house
-    // center: [0,0],
-    zoom: 3
-  })
+  layers: myLayers,  // all layers
+  view: myView
 });
 
+// adding a vector layer
+const vectorLayer = new VectorImageLayer({
+  source: new VectorSource({
+    url: './data/polygons.geojson',
+    format: new GeoJSON()  // specifying the format the data is in
+  }),
+  style: new Style({
+    fill: new Fill({
+      color: 'rgba(45, 184, 184,0.3)',
+    }),
+    stroke: new Stroke({
+      color: 'rgb(45, 184, 184)',
+      width: 2,
+    }),
+  }),
+  visible: true,
+  title: 'Calleva Park'
+});
+
+// adding the vectorLayer to the map (BUG: isnt showing up on map)
+map.addLayer(vectorLayer);
+
+
 // creating a marker (BUG: need to workout why its not showing up)
-const marker = new ol.layer.Vector({  // creates new vetor layer
-  source: new ol.source.Vector({  // new vector source
+const marker = new VectorLayer({  // creates new vetor layer
+  source: new VectorSource({  // new vector source
     features: [  // has list of features which include the geometry Point
-      new ol.Feature({
-        geometry: new ol.geom.Point(
-          ol.proj.fromLonLat([-1.162556, 51.360851])
+      new Feature({
+        geometry: new Point(
+          olProj.fromLonLat([-1.162556, 51.360851])
         )
       })
     ],
   }),
-  style: new ol.style.Style({  // need style inorder to create the marker icon from an image
-    image: new ol.style.Icon({
-      color: [113, 140, 0],
+  style: new Style({  // need style inorder to create the marker icon from an image
+    image: new Icon({
+      color: [45, 184, 184],
       crossOrigin: 'anonymous',
       src: 'images/pin-icon.png',  // url to the image
-      // anchor: [0.5, 1],  // halfway into the image and at the bottom (anchor coors start top left corner)
-      imgSize: [840,841],
+      anchor: [0.5, 1],  // halfway into the image and at the bottom (anchor coors start top left corner)
+      imgSize: [20,20],
     })
   })
 });
 
 // making the marker visible
-map.addLayer(marker)
+map.addLayer(marker);
